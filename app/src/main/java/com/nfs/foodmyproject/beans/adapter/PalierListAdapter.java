6fb9a8dc5b31f4dff92e85f24654cbba93f163f9
Baptiste Angot <bat76640@gmail.com>
2022-06-07 13:48:19 +0200

    Create detail page

---
+++ app/src/main/java/com/nfs/foodmyproject/beans/adapter/PalierListAdapter.java
@@ -0,0 +1,58 @@
+package com.nfs.foodmyproject.beans.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.nfs.foodmyproject.R;
+import com.nfs.foodmyproject.beans.Palier;
+
+import java.util.List;
+
+public class PalierListAdapter extends BaseAdapter {
+    private Context context;
+    private List<Palier> paliers;
+
+    public PalierListAdapter(Context context, List<Palier> paliers) {
+        this.context = context;
+        this.paliers = paliers;
+    }
+
+    @Override
+    public int getCount() {
+        return paliers.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return paliers.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).
+                    inflate(R.layout.palier, parent, false);
+        }
+
+        Palier palier = (Palier) getItem(position);
+        TextView titre = (TextView) convertView.findViewById(R.id.titre);
+        TextView description = (TextView) convertView.findViewById(R.id.description);
+        TextView price = (TextView) convertView.findViewById(R.id.price);
+        TextView date = (TextView) convertView.findViewById(R.id.date);
+
+        titre.setText(palier.getTitle());
+        description.setText(palier.getDescription());
+        price.setText(palier.getPrice() + " €");
+        date.setText(palier.getDateEnvoi().toString());
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/nfs/foodmyproject/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/nfs/foodmyproject/ui/dashboard/DashboardViewModel.java
@@ -1,5 +1,8 @@
 package com.nfs.foodmyproject.ui.dashboard;
 
+import android.widget.ImageView;
+import android.widget.ListView;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -8,12 +11,33 @@ public class DashboardViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
+    private final MutableLiveData<String> description;
+
+    private final MutableLiveData<ImageView> image;
+
+    private final MutableLiveData<ListView> mListView;
+
     public DashboardViewModel() {
+        mListView = new MutableLiveData<>();
         mText = new MutableLiveData<>();
+        description = new MutableLiveData<>();
+        image = new MutableLiveData<>();
         mText.setValue("This is dashboard fragment");
     }
 
     public LiveData<String> getText() {
         return mText;
+    }
+
+    public LiveData<ListView> getListView() {
+        return mListView;
+    }
+
+    public MutableLiveData<String> getDescription() {
+        return description;
+    }
+
+    public MutableLiveData<ImageView> getImage() {
+        return image;
     }
 }
